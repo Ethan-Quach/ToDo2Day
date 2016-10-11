5c9b7222b7695dd9861d621fad0d65a26069ecda
Ethan <ethan.quach.10@gmail.com>
2016-10-11 16:31:23 -0700

    Created new method getAllTasks() in DBHelper.java to retrieve tasks from database (currently only outputting as Log.i messages) and modified MainActivity.java to accomodate for current functionality. Layout coming soon!

--- app/src/main/java/edu/orangecoastcollege/cs273/equach3/todo2day/DBHelper.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/equach3/todo2day/DBHelper.java
@@ -1,14 +1,18 @@
 package edu.orangecoastcollege.cs273.equach3.todo2day;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 class DBHelper extends SQLiteOpenHelper {
 
     //TASK 1: DEFINE THE DATABASE VERSION, NAME AND TABLE NAME
     private static final String DATABASE_NAME = "ToDo2Day";
-    private static final String DATABASE_TABLE = "Tasks";
+    static final String DATABASE_TABLE = "Tasks";
     private static final int DATABASE_VERSION = 1;
 
 
@@ -39,4 +43,61 @@ class DBHelper extends SQLiteOpenHelper {
         onCreate(database);
     }
 
+    public void addTask(Task newTask)
+    {
+        // Step 1: Create a reference to our database
+        // this refers to DBHelper, which is a SQLiteOpenHelper.
+        // DBHelper already has information about the database itself (i.e. ToDo2Day)
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Step 2: Create a key-value pair for each value you want to insert
+        ContentValues values = new ContentValues();
+        values.put(KEY_FIELD_ID, newTask.getId());
+        values.put(FIELD_DESCRIPTION, newTask.getDescription());
+        values.put(FIELD_IS_DONE, newTask.getIsDone());
+
+        // Step 3: Insert the value into the database
+        db.insert(DATABASE_TABLE, null, values);
+
+        // Step 4: Always remember to close the database!
+        db.close();
+    }
+
+    // Creating a method to get all tasks in database:
+    public ArrayList<Task> getAllTasks()
+    {
+        // Step 1: Create reference to database
+        // Note that we use getReadableDatabase() since we aren't modifying records
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Step 2: Make a new empty ArrayList
+        ArrayList<Task> allTasks = new ArrayList<>();
+
+        // Step 3: Query the database for all records (all rows) and all fields (all columns)
+        // The return type of a query is Cursor
+        Cursor results = db.query(DATABASE_TABLE,
+                null, null, null, null, null, null);
+        // Uh... You can use null as a return type if you don't want to restrict anything.
+        // The first null is which fields we want to get back, sent as a String array.
+        // Will have to look into this later.
+
+        // Step 4: Loop through results, creating Task objects, and adding to ArrayList;
+        // moveToFirst() checks if the first entry is there (i.e. if entries exist at all)
+        if (results.moveToFirst())
+        {
+            do {
+                // getType(int i) returns a value of a row, taking in the column index as the param.
+                int id = results.getInt(0);
+                String description = results.getString(1);
+                int isDone = results.getInt(2);
+
+                allTasks.add(new Task(id, description, isDone));
+            } while (results.moveToNext());
+        }
+
+        // Step 4: Close the database!
+        db.close();
+
+        return allTasks;
+    }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/equach3/todo2day/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/equach3/todo2day/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.orangecoastcollege.cs273.equach3.todo2day;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FOR NOW (TEMPORARY), delete the old database, then create a new one
+        this.deleteDatabase(DBHelper.DATABASE_TABLE);
+
+        DBHelper db = new DBHelper(this);
+
+        // Creating a new task and adding it to the database:
+        db.addTask(new Task(1, "Get Mechanic character to Level 100", 0));
+        // (Hooray for anonymous inner classes!)
+        db.addTask(new Task(2, "Get Wild Hunter character to Level 100", 0));
+        db.addTask(new Task(3, "Maybe actually look at the CS273 book", 0));
+        db.addTask(new Task(4, "Get the Halloween Mercy skin", 0));
+        db.addTask(new Task(5, "Write Mahou Shoujo no 420", 0));
+
+        // Getting all the tasks from the database and printing them using Log.i
+        ArrayList<Task> allTasks = db.getAllTasks();
+        // Loop through each task and print to Log.i
+        for (Task t : allTasks) {
+            Log.i("DATABASE TASK", t.toString());
+        }
     }
 }
